475d6d21a34ebdd0ce9f0cf1d748b047ed98b825
Caio Barros <cgbarros@users.noreply.github.com>
2022-10-07 13:52:44 +0000

    exercises 3.7-3.11

--- main.scm
+++ main.scm
@@ -264,3 +264,77 @@
 			(cond ((eq? m 'generate) (generate))
 						((eq? m 'reset) reset)))
 		dispatch))
+
+;; Exercise 3.7: Consider the bank account objects created by make-account, with the password modification described in Exercise 3.3. Suppose that our banking system requires the ability to make joint accounts. Define a procedure make-joint that accomplishes this. Make-joint should take three arguments. The first is a password-protected account. The second argument must match the password with which the account was defined in order for the make-joint operation to proceed. The third argument is a new password. Make-joint is to create an additional access to the original account using the new password. For example, if peter-acc is a bank account with password open-sesame, then
+
+;; (define paul-acc
+;;   (make-joint peter-acc 
+;;               'open-sesame 
+;;               'rosebud))
+;; will allow one to make transactions on peter-acc using the name paul-acc and the password rosebud. You may wish to modify your solution to Exercise 3.3 to accommodate this new feature.
+
+(define (make-account password balance)
+	(let ((counter 0))
+	  (define (withdraw amount)
+	    (if (>= balance amount)
+	        (begin (set! balance 
+	                     (- balance amount))
+	               balance)
+	        "Insufficient funds"))
+	  (define (deposit amount)
+	    (set! balance (+ balance amount))
+	    balance)
+		(define (incorrect-password)
+			(if (< counter 7)
+					(begin (set! counter (++ counter))
+								  (error "Incorrect password"))
+					(call-the-cops)))
+	  (define (dispatch p m)
+			(if (eq? p password)
+					(begin (set! counter 0)
+								 (cond ((eq? m 'withdraw) withdraw)
+								  	   ((eq? m 'deposit) deposit)
+											 ((eq? m 'verify) #t)
+											 (else (error "Unknown request: 
+											 			 MAKE-ACCOUNT" m))))
+;					(lambda (m) (incorrect-password))))
+					(incorrect-password)))
+	  dispatch))
+
+(define (make-joint account-name oldpass newpass)
+	(let ((joint-try (account-name oldpass 'verify)))
+		(if joint-try
+				(lambda (pwd m)
+					(if (eq? pwd newpass)
+							(account-name oldpass m)
+							(error "Invalid access to joint account")))
+				'())))
+
+;; (define peter-acc (make-account 'open-sesame 100))
+;; (display ((peter-acc 'open-sesame 'withdraw) 10))
+;; (newline)
+;; (display (peter-acc 'open-sesame 'verify))
+;; (newline)
+
+;; (define paul-acc (make-joint peter-acc 'open-sesame 'rosebud))
+;; (display ((paul-acc 'rosebud 'withdraw) 10))
+;; (newline)
+;; (display ((paul-acc 'wrong-pass 'withdraw) 10))
+;; (newline)
+;; (display ((peter-acc 'rosebud 'withdraw) 10))
+
+;; Exercise 3.8: When we defined the evaluation model in 1.1.3, we said that the first step in evaluating an expression is to evaluate its subexpressions. But we never specified the order in which the subexpressions should be evaluated (e.g., left to right or right to left). When we introduce assignment, the order in which the arguments to a procedure are evaluated can make a difference to the result. Define a simple procedure f such that evaluating
+
+;; (+ (f 0) (f 1))
+
+;; will return 0 if the arguments to + are evaluated from left to right but will return 1 if the arguments are evaluated from right to left.
+
+;; after many tries
+
+(define f
+	(let ((x 0))
+		(lambda (y)
+			(if (= y 0)
+					x
+					(begin (set! x y)
+								 (- x y))))))
